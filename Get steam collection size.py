ef57d7fc866d18abe74ebdf72d0221c77cb187fd
Neko <lukegotjellyfish@gmail.com>
2019-02-19 19:08:35 +0000

    update

--- Get steam collection size.py
+++ Get steam collection size.py
@@ -5,66 +5,70 @@ from lxml import html
 import requests
 import fnmatch
 
-while True:
-    try:
-        input_url = input("Enter collection URL: ")
+sizes = []
+input_url = []
+with open("Collections.txt") as url_file:
+    for line in url_file:
+        input_url.append(line)
 
-        req = Request(input_url)
-        html_page = urlopen(req)
-        soup = BeautifulSoup(html_page, "lxml")
+for i in range(0, len(input_url)-1):
+    req = Request(input_url[i])
+    html_page = urlopen(req)
+    soup = BeautifulSoup(html_page, "lxml")
 
-        links = []
-        x = 2
+    links = []
+    x = 2
 
-        for link in soup.findAll('a', class_=False, href=True, target=False):  #get links and filter out unwanted class and target html
-            if x%2 == 0:
-                links.append(str(link.get('href')))
-            x += 1
-            
-        links = fnmatch.filter(links, 'https://steamcommunity.com/sharedfiles/filedetails/?id=*')
-        print("Addon Count: " + str(len(links)) + "\n")
+    for link in soup.findAll('a', class_=False, href=True, target=False):  #get links and filter out unwanted class and target html
+        if x%2 == 0:
+            links.append(str(link.get('href')))
+        x += 1
+        
+    links = fnmatch.filter(links, 'https://steamcommunity.com/sharedfiles/filedetails/?id=*')
+    print("Addon Count: " + str(len(links)) + "\n")
 
 
-        #go through each mod in collection to get filesize
-        x = 1
-        link_bank = []
-        total_size = 0
+    #go through each mod in collection to get filesize
+    x = 1
+    link_bank = []
+    total_size = 0
 
-        if (len(links) > 100):
-            spacer = " "  #Make the output look more... a e s t h e t i c
+    if (len(links) > 100):
+        spacer = " "  #Make the output look more... a e s t h e t i c
+    else:
+        spacer = "" 
+
+    for i in links:
+        url = i
+
+        try:
+            if link_bank.index(url):
+                continue  #ignore duplicate link (if this somehow happens, just in case)
+
+        except:
+            link_bank.append(str(i))  #add to list of unique links
+            #at least it works
+
+        page = requests.get(url)
+        tree = html.fromstring(page.content)
+        file_size = tree.xpath('//div[@class="detailsStatRight"]/text()')
+        file_size = (file_size[0])[:-3].replace(",", "")
+        total_size += Decimal(file_size)
+        
+        if x < 10:
+            print(" " + spacer + str(x) + "| Running total = " + str(total_size))
+        elif x < 100:
+            print(spacer + str(x) + "| Running total = " + str(total_size))
         else:
-            spacer = "" 
+            print(str(x) + "| Running total = " + str(total_size))
+        x += 1
+    
+    sizes.append(total_size)
+    print("\nTotal = " + format(total_size) + " MB")
+    print("\n\n\n\n\n\n\n\n\n")
 
+print("Collection sizes in written order:\n" + "\n".join(sizes))
+print("Total size of all collections: " + str(sum(sizes)))
 
-
-        for i in links:
-            url = i
-
-            try:
-                if link_bank.index(url):
-                    continue  #ignore duplicate link (if this somehow happens, just in case)
-
-            except:
-                link_bank.append(str(i))  #add to list of unique links
-                #at least it works
-
-            page = requests.get(url)
-            tree = html.fromstring(page.content)
-            file_size = tree.xpath('//div[@class="detailsStatRight"]/text()')
-            file_size = (file_size[0])[:-3].replace(",", "")
-            total_size += Decimal(file_size)
-            
-            if x < 10:
-                print(" " + spacer + str(x) + "| Running total = " + str(total_size))
-            elif x < 100:
-                print(spacer + str(x) + "| Running total = " + str(total_size))
-            else:
-                print(str(x) + "| Running total = " + str(total_size))
-            x += 1
-            
-        print("\nTotal = " + format(total_size) + " MB")
-        print("\n\n\n\n\n\n\n\n\n")
-
-    except:
-        print("an error occured, try again...\n\n")
-        continue
+while True:
+    x = input("Paused")
